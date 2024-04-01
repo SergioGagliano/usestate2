b408a459034023eb09e622fd79fea9be3a61e0b2
Sergio Gagliano <sergiogagliano8@gmail.com>
2024-04-01 20:18:40 +0200

    esercizio completato

---
+++ stato es2/src/App.jsx
@@ -0,0 +1,13 @@
+import { Counter } from './Counter'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Counter startCounter={0} incrementCounter={1} decrementCounter={1}/>
+    </>
+  )
+}
+
+export default App
---
+++ stato es2/src/Counter.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { CounterDisplay } from "./CounterDisplay"
+
+export function Counter({startCounter, incrementCounter, decrementCounter}){
+    
+  let [counter, setCounter] = useState(startCounter)
+ 
+  let setter= ()=>{
+       setCounter((value) => value + incrementCounter)
+  }
+    
+  let decrement= ()=>{
+    const dec= 0
+    if(dec < counter ){
+      setCounter((value) => value - decrementCounter)  
+    }
+  }
+
+  let reset= ()=>{
+    setCounter((value) => value = startCounter)
+  }
+
+    return(
+        <>
+        <CounterDisplay count={counter}/>
+        <button onClick={setter}>Aumenta valore</button>
+        <button onClick={decrement}>Diminuisci valore</button>
+        <button onClick={reset}>Azzera valore</button>
+        </>
+    ) 
+}
